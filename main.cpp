5f617ed1ce59bfad3d8084449b34c58731b71d9e
PhanKiet <tuankietvn@outlook.com>
2020-09-01 14:15:53 +0700

    stage 1: done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <cstdio>
+#include "libs/picosha2.h"
+#include <vector>
+
+#include "elements/chain.cpp"
 
 using namespace std;
 
 int main()
 {
-    cout << "Cool";
+    Chain PKcoin;
 }
